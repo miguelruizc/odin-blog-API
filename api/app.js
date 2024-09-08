6cbd34a6b770091f21de08956b0fd33dd4f4de74
miguelruizc <ruizconde.m@gmail.com>
2024-09-08 18:47:56 +0200

    Add CORS configuration (API) and login route (editors frontend)

--- api/app.js
+++ api/app.js
@@ -2,13 +2,34 @@ const express = require('express');
 const blogsRouter = require('./routes/blogs');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// CORS Configuration
+const allowedOrigins = [
+	process.env.DEVELOPMENT_PC,
+	process.env.EDITORS_FRONTEND,
+	process.env.READERS_FRONTEND,
+];
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (!origin)
+			console.log('*---Attempt to access API from a source without (CORS) origin header');
+		if (origin && allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			console.log('*---Attempt to access API from a source not allowed by CORS');
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+};
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/blogs', blogsRouter);
--- editors-frontend/src/login.jsx
+++ editors-frontend/src/login.jsx
@@ -1,5 +1,68 @@
 function Login() {
-	return <h1>Log In</h1>;
+	// Submit form function
+	const submitForm = async (event) => {
+		event.preventDefault();
+
+		// Send form data to API
+		const formData = new FormData(event.target);
+		const username = formData.get('username');
+		const password = formData.get('password');
+
+		try {
+			const response = await fetch('https://blogapi.miguelruizc.xyz/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					username,
+					password,
+				}),
+			});
+
+			// Response NOT OK
+			if (!response.ok) {
+				throw new Error(response.statusText);
+			}
+
+			// Response OK
+			const result = await response.json();
+			alert(result);
+		} catch (error) {
+			// Handle error messages
+			alert(error.message);
+		}
+	};
+
+	// JSX elements
+	return (
+		<div className="main login">
+			<form onSubmit={submitForm}>
+				<div>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						name="username"
+						// pattern="^[a-zA-Z0-9]+$"
+						// title="Special characters and spaces are not allowed"
+						// required
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						// pattern="^[a-zA-Z0-9]+$"
+						// title="Special characters and spaces are not allowed"
+						// required
+					/>
+				</div>
+				<button type="submit">Log In</button>
+				<div className="errors"></div>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
