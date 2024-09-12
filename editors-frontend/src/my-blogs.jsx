0678683dca82bc9307334fb59f26d8485dc65cf8
miguelruizc <ruizconde.m@gmail.com>
2024-09-12 20:25:19 +0200

    Add incomplete blogs route

--- editors-frontend/src/my-blogs.jsx
+++ editors-frontend/src/my-blogs.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 function MyBlogs() {
+	const [blogs, setBlogs] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	// Fetch data on mount
+	useEffect(() => {
+		fetch('blogapi.miguelruizc.xyz/blogs')
+			.then((response) => {
+				if (!response.ok) throw new Error('Something went wrong');
+				return response.json();
+			})
+			.then((data) => {
+				setBlogs(data);
+				setLoading(false);
+			})
+			.catch((error) => {
+				setError(error);
+				setLoading(false);
+			});
+	}, []);
+
 	// User not authenticated
 	if (!localStorage.getItem('jwt')) return <Navigate to="/login" />;
 
 	// JSX elements
-	return <div className="main blogs">List blogs</div>;
+	console.log('blogs: ', blogs);
+	console.log('error: ', error);
+	return (
+		<div className="main blogs">
+			{loading && <p>Loading...</p>}
+			{error && <p>{JSON.stringify(error)}</p>}
+			{blogs && <p>{JSON.stringigy(blogs)}</p>}
+		</div>
+	);
 }
 
 export default MyBlogs;
